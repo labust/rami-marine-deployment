bcfdb6be5ca5d950108260ed87a1748471b95f57
BartulK <bartul.kajmak56@gmail.com>
2024-11-08 11:44:59 +0100

    Add files via upload

---
+++ OCR/test_script.py
@@ -0,0 +1,13 @@
+import os
+from paddleocr import PaddleOCR
+ocr = PaddleOCR(lang='en') # need to run only once to load model into memory
+dir_path = r"C:\Users\Makedonski ajvar\Downloads\images"
+
+for img_name in os.listdir(dir_path):
+    img_path = dir_path + '\\' + img_name
+    print(img_name)
+    result = ocr.ocr(img_path, det=False, cls=False)
+    for idx in range(len(result)):
+        res = result[idx]
+        for line in res:
+            print(line)
