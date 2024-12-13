23cc1d152818188397b5b8d3f110d6a7c7078d9a
BartulK <bartul.kajmak56@gmail.com>
2024-12-13 10:43:48 +0100

    Update test_script.py

--- OCR/test_script.py
+++ OCR/test_script.py
@@ -1,6 +1,6 @@
 import os
 from paddleocr import PaddleOCR
-ocr = PaddleOCR(lang='en') # need to run only once to load model into memory
+ocr = PaddleOCR(lang='en', rec_model_dir="C:/Users/Makedonski ajvar/PaddleOCR/tools/output/test7/best") # need to run only once to load model into memory
 dir_path = r"C:\Users\Makedonski ajvar\Downloads\images"
 
 for img_name in os.listdir(dir_path):
