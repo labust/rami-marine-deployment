12117cc79e2556d853954ab82539850001cc5655
BartulK <bartul.kajmak56@gmail.com>
2025-01-20 17:57:14 +0100

    Update ocr_script.py

--- OCR/ocr_script.py
+++ OCR/ocr_script.py
@@ -1,10 +1,10 @@
 import os
 from paddleocr import PaddleOCR
 
-def process_ocr(image_path, model_dir):
+def process_ocr(cropped_image, model_dir):
     ocr = PaddleOCR(lang='en', rec_model_dir=model_dir, show_log = False)
 
-    ocr_results = ocr.ocr(image_path, det=False, cls=False)
+    ocr_results = ocr.ocr(cropped_image, det=False, cls=False)
 
     for res in ocr_results:
         for line in res:
