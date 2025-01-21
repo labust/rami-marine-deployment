30abeb2f3f18b84424ae45d586f803a40a1c42a2
BartulK <bartul.kajmak56@gmail.com>
2025-01-21 19:06:16 +0100

    Update ocr_script.py

--- OCR/ocr_script.py
+++ OCR/ocr_script.py
@@ -1,9 +1,11 @@
 from paddleocr import PaddleOCR
+from numpy import asarray
 
 def process_ocr(cropped_image, model_dir):
     ocr = PaddleOCR(lang='en', rec_model_dir=model_dir, show_log = False)
+    arr_image = asarray(cropped_image)
 
-    ocr_results = ocr.ocr(cropped_image, det=False, cls=False)
+    ocr_results = ocr.ocr(arr_image, det=False, cls=False)
 
     for res in ocr_results:
         for line in res:
