8e7eb9cf28e69bd6d67c1b969344878c799db1e6
lovrerancev <72494151+lovrerancev@users.noreply.github.com>
2025-01-29 23:35:18 +0100

    Add files via upload
    Popravljen ispis

--- Lovre/zavrsnaSkripta/Class5Det.py
+++ Lovre/zavrsnaSkripta/Class5Det.py
@@ -1,28 +1,35 @@
 from ultralytics import YOLO
-#imena klasa su cross-valve i ringpole
-# Učitavanje YOLO modela
-model_path = "class5.pt"  # Putanja do vašeg modela
+
+
+model_path = "class5.pt"
 model = YOLO(model_path)
 
-def class5_detection(image):
 
+class_mapping = {
+    "cross-valve": "x-shaped valve",
+    "ringpole": "ring pole"
+}
+
+def class5_detection(image):
     # Provjera je li slika ispravno učitana
     if image is None:
         print("Greška: Slika nije učitana!")
         return None
 
-    # Detekcija objekata pomoću YOLO modela
-    results = model.predict(source=image, conf=0.5, save=False)  # confidence threshold = 0.5
-    detections = results[0].boxes  # Bounding boxes
 
-    # Provjera postoji li barem jedna detekcija
+    results = model.predict(source=image, conf=0.5, save=False)
+    detections = results[0].boxes
+
+
     if len(detections) == 0:
         print("Nema detektiranih objekata na slici.")
         return None
 
-    # Pronalazak klase s najvećom vjerovatnošću
-    best_detection = max(detections, key=lambda det: det.conf[0])  # Najveća vjerovatnoća
-    class_id = int(best_detection.cls[0])  # ID klase
-    class_name = model.names[class_id]  # Naziv klase
 
-    return class_name
+    best_detection = max(detections, key=lambda det: det.conf[0])
+    class_id = int(best_detection.cls[0])
+    class_name = model.names[class_id]
+
+    mapped_class_name = class_mapping.get(class_name, class_name)
+
+    return mapped_class_name
